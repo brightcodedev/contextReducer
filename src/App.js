29c199c84696a113d719c46da5bebb08d42820fa
brightcodedev <info@brightcode.dev>
2020-03-26 16:55:30 -0400

    Start file

---
+++ src/App.js
@@ -0,0 +1,9 @@
+///////// Starter ///////////
+import React from 'react';
+function App() {
+  return (
+    <h1>Bright Code For Life!</h1>
+  );
+}
+export default App;
+///////// Starter ///////////
