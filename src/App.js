d22c4b03206d0a2e1d568bb28cf2eb48b7c2c28e
brightcodedev <info@brightcode.dev>
2020-03-26 19:13:00 -0400

    Covered Reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-///////// Starter ///////////
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+
+const reducer=(state, action)=>{
+  switch(action.type){
+    case 'INCREMENT':
+      return {count: state.count + 1}
+    case 'DECREMENT':
+      return {count: state.count -1}
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count:0});
+  // const [count, setCount] = useState(0);
   return (
-    <h1>Bright Code For Life!</h1>
+    <>
+      Count: {state.count}
+      {/* Count: {count} */}
+      <button onClick={()=>dispatch({ type: 'INCREMENT' })}>+</button>
+      {/* <button onClick={()=>setCount(count+1)}>+</button> */}
+      <button onClick={()=>dispatch({ type: 'DECREMENT' })}>-</button>
+      {/* <button onClick={()=>setCount(count-1)}>-</button> */}
+    </>
   );
 }
 export default App;
-///////// Starter ///////////
