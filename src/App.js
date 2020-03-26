7fcb5e17b2789d0dd9447d4d505c459177b7deaa
brightcodedev <info@brightcode.dev>
2020-03-26 19:40:31 -0400

    Added reducer with context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer, useContext, createContext, useState } from 'react';
+import { findAllByDisplayValue } from '@testing-library/react';
 
 const reducer=(state, action)=>{
   switch(action.type){
     case 'INCREMENT':
       return {count: state.count + 1}
     case 'DECREMENT':
-      return {count: state.count -1}
+      return {count: state.count - 1}
+    case 'RESET':
+      return {count: 0}
+    case 'INCREMENTBYCUSTOM':
+      return {count: state.count + action.payload}
     default:
       throw new Error();
   }
 }
 
+const CountContext = createContext();
+
+function Counter(){
+    const [state, dispatch] = useContext(CountContext);
+    const [countInput, setCountInput] = useState(0);
+    return(
+      <>
+        Count: {state.count}
+        <button onClick={()=>dispatch({ type: 'INCREMENT' })}>+</button>
+        <button onClick={()=>dispatch({ type: 'DECREMENT' })}>-</button>
+        <button onClick={()=>dispatch({ type: 'RESET' })}>reset</button>
+        <br />
+        <input onChange={(e)=>setCountInput(Number(e.target.value))} placeholder="Give me an incrementor" />
+        <button onClick={()=>dispatch({ type: 'INCREMENTBYCUSTOM', payload: countInput })}>Increment Now!</button>
+
+      </>
+    )
+}
+
 function App() {
-  const [state, dispatch] = useReducer(reducer, {count:0});
-  // const [count, setCount] = useState(0);
+  const stateDispatchObj = useReducer(reducer, {count:0});
   return (
-    <>
-      Count: {state.count}
-      {/* Count: {count} */}
-      <button onClick={()=>dispatch({ type: 'INCREMENT' })}>+</button>
-      {/* <button onClick={()=>setCount(count+1)}>+</button> */}
-      <button onClick={()=>dispatch({ type: 'DECREMENT' })}>-</button>
-      {/* <button onClick={()=>setCount(count-1)}>-</button> */}
-    </>
+    <CountContext.Provider value={stateDispatchObj}>
+      <Counter />
+    </CountContext.Provider>
+
   );
 }
 export default App;
